dfaf7d26d3c3f5184837be29708b425c3e59af58
David Umoru <umorudavido@gmail.com>
2025-04-08 11:48:15 +0100

    Remove CTASection component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
-import CTASection from "./components/CTASection";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <main className="flex-1">
         <Hero />
         <Features />
-        <CTASection />
       </main>
       <Footer />
     </>
